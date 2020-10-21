f3d80d50fdfaccc04b3bd6d5f26c1a7d80b1b392
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2020-10-21 22:40:55 +0200

    Add files via upload

---
+++ ukolDatabaze.sql
@@ -0,0 +1,8 @@
+ALTER TABLE cars MODIFY COLUMN id_car INT NOT NULL  AUTO_INCREMENT PRIMARY KEY; 
+ALTER TABLE employees MODIFY COLUMN id_employee INT NOT NULL  AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE garages add column email int FIRST;  
+ALTER TABLE garages add column phone int FIRST; 
+ALTER TABLE garages add column název int FIRST; 
+ALTER TABLE garages add column capacity int FIRST;
+ALTER TABLE garages add column description int FIRST;
+ALTER TABLE garages add column GPS  int FIRST;
