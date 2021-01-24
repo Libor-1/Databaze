89adedb1e447fa331f1b3f0792ee2b2ec7f1d8c7
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2021-01-24 17:21:36 +0100

    Create countries.sql

---
+++ countries.sql
@@ -0,0 +1,29 @@
+INSERT INTO countries (
+  name,
+  code,
+  description
+)
+VALUES ('Polsko','PL',NULL),
+       ('Španělsko', 'ES',NULL),
+       ('Rusko', 'RU',NULL),
+       ('Finsko', 'FI',NULL),
+       ('Itálie', 'IT',NULL),
+       ('Řecko', 'GR',NULL),
+       ('Barbados', 'BB',NULL),
+       ('Brazílie', 'BR',NULL),
+       ('Dánsko', 'DK',NULL),
+       ('Diminikánská republika', 'DO',NULL),
+       ('Egypt', 'EG',NULL),
+       ('Ekvádor', 'EC',NULL),
+       ('Francie', 'FR',NULL),
+       ('Chile','CL',NULL),
+       ('Irák', 'IQ',NULL),
+       ('Jamajka', 'JM',NULL),
+       ('Izrael', 'IL',NULL),
+       ('Česká republika', 'CZ',NULL),
+       ('Kostarika', 'CR',NULL),
+       ('Madagaskar', 'MG',NULL);
+       
+
+
+       
