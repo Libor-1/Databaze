7b3d34ecc70d29207e982b82074d8cd3e78c247a
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2021-02-15 21:11:32 +0100

    Create select_like.sql

---
+++ select_like.sql
@@ -0,0 +1,4 @@
+select * FROM countries WHERE name LIKE 'C%';
+select * FROM countries WHERE name LIKE '%a';
+select * FROM countries WHERE name LIKE 'republic' or 'Republic';
+select * FROM countries WHERE name LIKE '%ra%';
